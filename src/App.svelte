43cc32047ea127b353233203c241e6b93c91cf68
szuhaydv <szuhaydv@gmail.com>
2024-09-22 19:56:17 +0200

    routing init

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,22 @@
 <script>
 	import Router from "svelte-spa-router";
 	import Home from "./routeViews/Home.svelte";
+	import PollCreator from "./routeViews/PollCreator.svelte";
+	import PollResults from "./routeViews/PollResults.svelte";
+	import PollLink from "./routeViews/PollLink.svelte";
 
 	const routes = {
 		"/": Home,
+		"/creator": PollCreator,
+		"/results": PollResults,
+		"/link": PollLink,
 	};
 </script>
 
 <Router {routes} />
+<a href="#/creator">Go to Creator</a>
+<a href="#/results">Go to Results</a>
+<a href="#/link">Go to Link</a>
 
 <style>
 	:global(body) {
---
+++ src/routeViews/PollResults.svelte
@@ -0,0 +1,1 @@
+<h1 class="font-actionJackson">Welcome to the Poll Results</h1>
