b47d3378740553ae041b8728d120c7ce85ec9438
szuhaydv <szuhaydv@gmail.com>
2024-09-22 16:36:49 +0200

    init livepoll project

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let name;
+</script>
+
+<svelte:head>
+	<link href="https://fonts.cdnfonts.com/css/action-jackson" rel="stylesheet">
+</svelte:head>
+
+<main>
+	<h1 class="text-8xl text-yellow-400 text-center mt-[20vh] font-actionJackson">Welcome<br>to livepoll!</h1>
+	
+</main>
+
+<style>
+	:global(body) { 
+		margin: 0;
+		padding: 0;
+		background-image: url('/assets/tile.png');
+		background-repeat: repeat;
+		background-size: auto;
+	}
+</style>
