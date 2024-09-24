7c4e382d07fab12844c6b3ec5e942d14fd427bbc
szuhaydv <szuhaydv@gmail.com>
2024-09-24 17:51:26 +0200

    setup backend routing + serving assets

--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 )
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	http.ServeFile(w, r, filepath.Join("..", "public", "index.html"))
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("../public/build")))
+	fsBuild := http.FileServer(http.Dir(filepath.Join("..", "public", "build")))
+	http.Handle("/build/", http.StripPrefix("/build/", fsBuild))
+
+	fsAssets := http.FileServer(http.Dir(filepath.Join("..", "public", "assets")))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fsAssets))
+
+	http.HandleFunc("/", testHandler)
 
 	fmt.Println("Running server...")
 	err := http.ListenAndServe(":7777", nil)
