c19eece158602a8855106f4636918f59df2884e1
szuhaydv <szuhaydv@gmail.com>
2024-09-24 15:10:25 +0200

    countdown works based on value

--- src/routeViews/PollResults.svelte
+++ src/routeViews/PollResults.svelte
@@ -2,6 +2,18 @@
 	import Note from "../components/Note.svelte";
 	const texts = ["Situps", "Push ups", "Squats", "Pull ups"];
 	const percentages = [22, 11, 33, 33];
+	$: duration = 5;
+	let intervalRef = null;
+	function tick() {
+		intervalRef = setInterval(() => {
+			if (duration == 0 && intervalRef) {
+				clearInterval(intervalRef);
+				return;
+			}
+			duration -= 1;
+		}, 1000);
+	}
+	tick();
 </script>
 
 <h1 class="text-8xl text-yellow-400 text-center pt-[4vh] font-actionJackson">
@@ -10,7 +22,9 @@
 <Note title="What exercise should I do?" titleMargin={2}>
 	<div class="w-full flex justify-center items-center gap-5 mb-8">
 		<img src="/assets/clock.svg" alt="Clock icon" />
-		<p>2:34</p>
+		<p>
+			{Math.floor(duration / 60)}:{String(duration % 60).padStart(2, "0")}
+		</p>
 	</div>
 	<ul class="flex flex-col">
 		{#each texts as text, index}
