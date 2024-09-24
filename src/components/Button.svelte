924c547823f2bc8f9cd17ed708accede9093f040
szuhaydv <szuhaydv@gmail.com>
2024-09-24 03:45:59 +0200

    added copy link functionality

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,11 +1,16 @@
 <script>
 	export let url;
 	export let text;
+	export let actionButton = false;
+	export let action;
+	const styles =
+		"text-white visited:text-white hover:no-underline hover:scale-105 rounded-3xl border-white border-4 bg-gradient-to-b from-lime-300 to-green-700 w-max px-16 inline-block shadow-lg focus:border-white active:scale-100 transition";
 </script>
 
-<a
-	href={url}
-	class="text-white visited:text-white hover:no-underline rounded-3xl border-white border-4 bg-gradient-to-b from-lime-300 to-green-700 w-max px-16 inline-block shadow-lg focus:border-white active:scale-105 transition"
->
-	{text}
-</a>
+{#if actionButton}
+	<button class={styles} on:click={action}>{text}</button>
+{:else}
+	<a href={url} class={styles}>
+		{text}
+	</a>
+{/if}
--- src/routeViews/PollLink.svelte
+++ src/routeViews/PollLink.svelte
@@ -1,21 +1,27 @@
 <script>
 	import Note from "../components/Note.svelte";
 	import Button from "../components/Button.svelte";
+	let link = "https://www.something.com";
+
+	function copyLink() {
+		navigator.clipboard
+			.writeText(link)
+			.catch((err) => console.log("Colud not copy text", err));
+	}
 </script>
 
 <h1 class="text-8xl text-yellow-400 text-center pt-[4vh] font-actionJackson">
 	Livepoll
 </h1>
 <Note title="Your poll is available at:">
-	<div
-		class="flex items-center justify-between pl-8 pr-4 py-1 bg-white rounded-2xl"
+	<button
+		class="flex items-center justify-between border-none w-full pl-8 pr-4 py-1 bg-white rounded-2xl cursor-pointer active:scale-95 transition"
+		on:click={copyLink}
 	>
-		<p class="font-caveatBrush text-5xl inline-block">
-			https://www.something.com
-		</p>
+		<span class="inline-block">{link}</span>
 		<img class="w-14 h-14" src="/assets/link.svg" alt="Link icon" />
-	</div>
+	</button>
 	<div class="w-full flex justify-center mt-10">
-		<Button text="Copy" url="https://www.something.com"></Button>
+		<Button text="Copy" actionButton="true" action={copyLink}></Button>
 	</div>
 </Note>
