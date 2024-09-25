0724a24bc970354eeabbd81789813cc0ab16b4f3
szuhaydv <szuhaydv@gmail.com>
2024-09-25 13:34:31 +0200

    added function executing insert of options

--- backend/handle_queries.go
+++ backend/handle_queries.go
@@ -8,19 +8,34 @@ import (
 	"github.com/google/uuid"
 )
 
-func insertPoll(db *sql.DB, duration string, title string) error {
+func insertPoll(db *sql.DB, poll Poll) (uuid.UUID, error) {
 	sqlBytes, err := os.ReadFile("./queries/insert_poll.sql")
 	if err != nil {
-		return fmt.Errorf("Error reading sql query for insert into polls. %w", err)
+		return uuid.Nil, fmt.Errorf("Error reading sql query for insert into polls. %w", err)
 	}
 
 	id := uuid.New()
 
-	_, err = db.Exec(string(sqlBytes), id, duration, title)
+	_, err = db.Exec(string(sqlBytes), id, poll.Duration, poll.Title)
 	if err != nil {
-		return fmt.Errorf("Error exectuing sql query for insert into polls. %w", err)
+		return uuid.Nil, fmt.Errorf("Error executing sql query for insert into polls. %w", err)
 
 	}
 
+	return id, nil
+}
+
+func insertOptions(db *sql.DB, options []Option) error {
+	for _, option := range options {
+		sqlBytes, err := os.ReadFile("./queries/insert_option.sql")
+		if err != nil {
+			fmt.Errorf("Error reading sql query for insert into options. %w", err)
+		}
+
+		_, err = db.Exec(string(sqlBytes), option.Name)
+		if err != nil {
+			fmt.Errorf("Error executing sql query for insert into options. %w", err)
+		}
+	}
 	return nil
 }
