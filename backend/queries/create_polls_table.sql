fc41b38177fd6f412d7e45654ba9e27b52dd7bdb
szuhaydv <szuhaydv@gmail.com>
2024-09-25 20:09:32 +0200

    changed polls column to created_at

--- backend/queries/create_polls_table.sql
+++ backend/queries/create_polls_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS polls (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     duration INTERVAL DEFAULT '120 second',
     title TEXT NOT NULL
 );
