78487c9d31a9bdb0af90877f3334cd57287bab6d
szuhaydv <szuhaydv@gmail.com>
2024-09-26 14:35:02 +0200

    fixed ambiguity and already exists errors for db func and trigger queries

--- backend/queries/function_create.sql
+++ backend/queries/function_create.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE FUNCTION notify_new_vote()
 RETURNS TRIGGER AS $$
 DECLARE
-    poll_id UUID;
+    updated_poll_id UUID;
     votes_count INT;
 BEGIN
-    SELECT poll_id INTO poll_id FROM options WHERE id = NEW.id;
+    SELECT poll_id INTO updated_poll_id FROM options WHERE id = NEW.id;
     SELECT votes INTO votes_count FROM options WHERE id = NEW.id;
 
-    PERFORM pg_notify('poll_' || poll_id, json_build_object('option_id', NEW.id, 'votes', votes_count)::text);
+    PERFORM pg_notify('poll_' || NEW.poll_id, json_build_object('option_id', NEW.id, 'votes', votes_count)::text);
 
     RETURN NEW;
 END;
--- backend/queries/trigger_create.sql
+++ backend/queries/trigger_create.sql
@@ -1,3 +1,13 @@
-CREATE TRIGGER new_vote_trigger
-AFTER INSERT OR UPDATE ON options -- maybe add DELETE later
-FOR EACH ROW EXECUTE FUNCTION notify_new_vote();
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_trigger
+        WHERE tgname = 'new_vote_trigger'
+    ) THEN
+        CREATE TRIGGER new_vote_trigger
+        AFTER INSERT OR UPDATE ON options -- maybe add DELETE later
+        FOR EACH ROW EXECUTE FUNCTION notify_new_vote();
+    END IF;
+END;
+$$;
